37a37b3501b3ce7bf7e7d16756961865e79d1093
Woojin Choi <woojin.choi@bepro11.com>
2022-10-01 01:02:50 +0900

    Apply black.

--- conftest.py
+++ conftest.py
@@ -20,8 +20,12 @@ def get_weather():
         return Weather(
             hour_offset=fake.random_choices(elements=(0, -6, -12, -18, -24)) if hour_offset is None else hour_offset,
             status=fake.random_choices(elements=WeatherStatus) if status is None else status,
-            temperature=fake.pyfloat(min_value=-100.0, max_value=100.0, right_digits=2) if temperature is None else temperature,
-            precipitation=fake.pyfloat(positive=True, max_value=1000.0, right_digits=2) if precipitation is None else precipitation,
+            temperature=fake.pyfloat(min_value=-100.0, max_value=100.0, right_digits=2)
+            if temperature is None
+            else temperature,
+            precipitation=fake.pyfloat(positive=True, max_value=1000.0, right_digits=2)
+            if precipitation is None
+            else precipitation,
         )
 
     return _weather
