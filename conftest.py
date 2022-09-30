178e55880a60e88362421b87faa66050308c2667
Woojin Choi <woojin.choi@bepro11.com>
2022-10-01 01:01:46 +0900

    New GreetingStrategies test cases. fix bugs for conftest.

--- conftest.py
+++ conftest.py
@@ -18,10 +18,10 @@ def get_weather():
         precipitation: Optional[float] = None,
     ):
         return Weather(
-            hour_offset=hour_offset or fake.random_choices(elements=(0, -6, -12, -18, -24)),
-            status=status or fake.random_choices(elements=WeatherStatus),
-            temperature=temperature or fake.pyfloat(min_value=-100.0, max_value=100.0, right_digits=2),
-            precipitation=precipitation or fake.pyfloat(positve=True, max_value=1000.0, right_digits=2),
+            hour_offset=fake.random_choices(elements=(0, -6, -12, -18, -24)) if hour_offset is None else hour_offset,
+            status=fake.random_choices(elements=WeatherStatus) if status is None else status,
+            temperature=fake.pyfloat(min_value=-100.0, max_value=100.0, right_digits=2) if temperature is None else temperature,
+            precipitation=fake.pyfloat(positive=True, max_value=1000.0, right_digits=2) if precipitation is None else precipitation,
         )
 
     return _weather
