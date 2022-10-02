e18ae8d8dcdd17be3b1dcefd14c407c6f57b8d2f
Woojin Choi <woojin.choi@bepro11.com>
2022-10-02 12:24:40 +0900

    Implement service layer.

---
+++ src/services/services.py
@@ -0,0 +1,14 @@
+from typing import Dict
+
+from src.adapters.repository import AbstractWeatherSummaryRepository
+from src.domain.models import Location
+
+
+def get_weather_summary_messages(location: Location, repo: AbstractWeatherSummaryRepository) -> Dict[str, str]:
+    weather_summary = repo.get_weather_summary(location, message_strategy="default")
+    summary_messages = dict(
+        greeting=weather_summary.generate_greeting_message(),
+        temperature=weather_summary.generate_temperature_message(),
+        heads_up=weather_summary.generate_heads_up_message(),
+    )
+    return summary_messages
