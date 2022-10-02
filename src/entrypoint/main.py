11a2113f9472eddd4dcebfe0519a1fad3ddedb9d
Woojin Choi <woojin.choi@bepro11.com>
2022-10-02 13:41:12 +0900

    Handle 400 and 500 errors.

--- src/entrypoint/main.py
+++ src/entrypoint/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
 from src.adapters import repository
 from src.domain import models
@@ -7,9 +9,17 @@ from src.services import services
 app = FastAPI()
 
 
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return JSONResponse(status_code=400, content=str(exc))
+
+
 @app.get("/summary")
-async def summary(lat: float, lon: float):
-    repo = repository.DroomWeatherSummaryRepository()
-    location = models.Location(latitude=lat, longitude=lon)
-    summary_messages = await services.get_weather_summary_messages(location=location, repo=repo)
+async def summary(lat: float = Query(ge=-90, le=90), lon: float = Query(ge=-180, le=180)):
+    try:
+        repo = repository.DroomWeatherSummaryRepository()
+        location = models.Location(latitude=lat, longitude=lon)
+        summary_messages = await services.get_weather_summary_messages(location=location, repo=repo)
+    except Exception:
+        raise HTTPException(status_code=500)
     return dict(summary=summary_messages)
